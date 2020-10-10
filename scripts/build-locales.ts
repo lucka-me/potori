3fbfdca05f5599420360aee6eb4afa2f648ab824
lucka-me <samzhao0727@gmail.com>
2020-10-10 23:04:33 +0800

    Finish the new build-locales.ts

--- scripts/build-locales.ts
+++ scripts/build-locales.ts
@@ -1,104 +1,151 @@
 import * as fs from 'fs';
 
-type LocaleKey = string;
-type LocaleTranslation = string;
-type LocaleItem = [LocaleKey, LocaleTranslation];
-type LocaleNamespace = string;
+namespace locale {
+    type Key = string;
+    type Translation = string;
+    type Item = [Key, Translation];
+    type Namespace = string;
 
-class LocaleData {
+    class LocaleData {
 
-    code: string;
-    data: Map<LocaleNamespace, Array<LocaleItem>> = new Map();
+        code: string;
+        data: Map<Namespace, Array<Item>> = new Map();
 
-    constructor(code: string) {
-        this.code = code;
-    }
-
-    buildFiles(root: string) {
-        const dir = `${root}/${this.code}`;
-        if (!fs.existsSync(dir)) {
-            fs.mkdirSync(`dir`);
+        constructor(code: string) {
+            this.code = code;
         }
-        for (const [ns, items] of this.data) {
-            const value: any = {};
-            for (const [key, translation] of items) {
-                value[key] = translation;
+
+        build(root: string) {
+            const dir = `${root}/${this.code}`;
+            if (!fs.existsSync(dir)) {
+                fs.mkdirSync(`dir`);
             }
-            fs.writeFileSync(`${dir}/${ns}.json`, JSON.stringify(value, null, 4));
+            for (const [ns, items] of this.data) {
+                const value: any = {};
+                for (const [key, translation] of items) {
+                    value[key] = translation;
+                }
+                fs.writeFileSync(`${dir}/${ns}.json`, JSON.stringify(value, null, 4));
+            }
+        }
+
+        moduleName(ns: string) {
+            return `${ns}_${this.code.replace('-', '_')}`
+        }
+
+        get importCode(): string {
+            const code = [];
+            for (const ns of this.data.keys()) {
+                code.push(`import ${this.moduleName(ns)} from './${this.code}/${ns}.json';`);
+            }
+            return code.join('\n');
+        }
+
+        get exportCode() {
+            const code = [];
+            for (const ns of this.data.keys()) {
+                code.push(`${ns}: ${this.moduleName(ns)},`);
+            }
+            return '\n\n'
+            + `    '${this.code}': {\n        `
+            + code.join('\n        ')
+            + '\n'
+            + '    },'
         }
     }
 
-    moduleName(ns: string) {
-        return `${ns}_${this.code.replace('-', '_')}`
-    }
+    const data = new Map<string, LocaleData>();
 
-    get importCode(): string {
-        const code = [];
-        for (const ns of this.data.keys()) {
-            code.push(`import ${this.moduleName(ns)} from './${this.code}/${ns}.json';`);
-        }
-        return code.join('\n');
-    }
-
-    get exportCode() {
-        const code = [];
-        for (const ns of this.data.keys()) {
-            code.push(`${ns}: ${this.moduleName(ns)},`);
-        }
-        return '\n\n'
-        + `    '${this.code}': {\n        `
-        + code.join('\n        ')
-        + '\n'
-        + '    },'
-    }
-}
-
-function search(dir: string, target: string, callback: (path: string) => void) {
-    const items = fs.readdirSync(dir);
-    for (const item of items) {
-        const path = `${dir}/${item}`;
-        if (item === target) {
-            callback(path);
-            continue;
-        }
-        if (fs.statSync(path).isDirectory()) {
-            search(path, target, callback);
+    /**
+     * Search for target file recursively
+     * @param dir Search directory
+     * @param target Target filename
+     * @param callback Triggered when found
+     */
+    function search(dir: string, target: string, callback: (path: string) => void) {
+        const items = fs.readdirSync(dir);
+        for (const item of items) {
+            const path = `${dir}/${item}`;
+            if (item === target) {
+                callback(path);
+                continue;
+            }
+            if (fs.statSync(path).isDirectory()) {
+                search(path, target, callback);
+            }
         }
     }
-}
 
-function buildLocales() {
-    const localeDataMap = new Map<string, LocaleData>();
-    search('./src', 'locales.json', (path) => {
+    /**
+     * Parse data from `locales.json`
+     * @param path Path of the `locales.json`
+     */
+    function parse(path: string) {
         console.log(`Load locales from ${path}`);
-        const data = JSON.parse(fs.readFileSync(path, 'utf-8'));
-        const perfix = path.replace('./src/', '').replace('/locales.json', '').replace(/\//g, '.');
-        for (const [ns, items] of Object.entries(data)) {
-            // Namespace
-            for (const [key, translations] of Object.entries(items)) {
-                // Keys
-                for (const [lang, translation] of Object.entries(translations)) {
-                    // Languages
-                    if (!localeDataMap.has(lang)) {
-                        localeDataMap.set(lang, new LocaleData(lang));
+            const json = JSON.parse(fs.readFileSync(path, 'utf-8'));
+            const perfix = path
+                .replace('./src/', '')
+                .replace('/locales.json', '')
+                .replace(/\//g, '.');
+            for (const [ns, items] of Object.entries(json)) {
+                // Namespace
+                for (const [key, translations] of Object.entries(items)) {
+                    // Keys
+                    for (const [lang, translation] of Object.entries(translations)) {
+                        // Languages
+                        if (!data.has(lang)) {
+                            data.set(lang, new LocaleData(lang));
+                        }
+                        const localeData = data.get(lang);
+                        if (!localeData.data.has(ns)) {
+                            localeData.data.set(ns, []);
+                        }
+                        localeData.data.get(ns).push([
+                            `${perfix}.${key}`, translation as string
+                        ]);
                     }
-                    const localeData = localeDataMap.get(lang);
-                    if (!localeData.data.has(ns)) {
-                        localeData.data.set(ns, []);
-                    }
-                    localeData.data.get(ns).push([`${perfix}.${key}`, translation as string]);
                 }
             }
-        }
-    });
-    // Build to temp folder temporarily
-    const targetRoot = './src/locales-new';
-    if (!fs.existsSync(targetRoot)) {
-        fs.mkdirSync(targetRoot);
     }
-    for (const data of localeDataMap.values()) {
-        data.buildFiles(targetRoot);
+
+    /**
+     * Build the `index.ts`
+     * @param root Root directory
+     */
+    function buildIndex(root: string) {
+        const codesImport = [];
+        const codesExport = [];
+        for (const localeData of data.values()) {
+            codesImport.push(localeData.importCode);
+            codesExport.push(localeData.exportCode);
+        }
+        const code = '// This file is generated by scripts/build-locals.js\n'
+            + codesImport.join('\n')
+            + '\n\n'
+            + 'export default {'
+            + codesExport.join('')
+            + '\n\n'
+            + '};';
+        
+        fs.writeFileSync(`${root}/index.ts`, code);
+    }
+
+    /**
+     * Build locales files
+     * @see Inspired by [Turnip-Calculator](https://github.com/elxris/Turnip-Calculator/blob/master/scripts/i18n.js)
+     */
+    export function build() {
+        search('./src', 'locales.json', (path) => parse(path));
+        // Build to temp folder temporarily
+        const targetRoot = './src/locales-new';
+        if (!fs.existsSync(targetRoot)) {
+            fs.mkdirSync(targetRoot);
+        }
+        for (const localeData of data.values()) {
+            localeData.build(targetRoot);
+        }
+        buildIndex(targetRoot);
     }
 }
 
-buildLocales();
+locale.build();
