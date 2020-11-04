0505f71e22f4cf81866bc77c6ebd2a5ce710a0d2
lucka-me <i@lucka.moe>
2020-11-04 14:21:41 +0800

    Build contents for match dialog

--- src/ui/dialog/match/index.ts
+++ src/ui/dialog/match/index.ts
@@ -19,17 +19,22 @@ interface MatchDialogEvents {
  */
 class MatchDialog extends DialogPrototype {
 
+    private content: HTMLDivElement = null;
+
     events: MatchDialogEvents = {
         close: () => { },
     };
 
     render() {
+        this.content = eli.build('div', {
+            className: 'mdc-dialog__content flex-box-col',
+        });
         const element = DialogPrototype.buildDialog([
             eli.build('h2', {
                 className: 'mdc-dialog__title',
                 innerHTML: i18next.t('ui.dialog.match.title')
             }),
-            // Contents
+            this.content,
             eli.build('footer', {
                 className: 'mdc-dialog__actions',
             }, [
@@ -49,8 +54,85 @@ class MatchDialog extends DialogPrototype {
      */
     open(target: Nomination, candidate: Nomination) {
         if (!this.ctrl) this.render();
-        // TODO: Set the elements
+
+        this.content.innerHTML = '';
+        this.content.append(i18next.t('ui.dialog.match.desc'));
+        this.buildBlock(target);
+        this.buildBlock(candidate);
+
         this.ctrl.open();
+    }
+
+    private buildBlock(nomination: Nomination) {
+        const classNameInfo = [
+            'margin-r--8',
+            'flex-box-row--nowrap',
+            'flex-align-items--center',
+        ].join(' ');
+        const info = [];
+        info.push(eli.build('span', {
+            className: classNameInfo,
+        }, [
+            eli.icon('&#xf062'),
+            eli.build('span', {
+                className: 'margin-l--4',
+                innerHTML: nomination.confirmedTime > 0 ? nomination.confirmedDateString : i18next.t('ui.dialog.match.missing'),
+            }),
+        ]));
+        if (nomination.status.code > 0) {
+            const type = service.status.getTypeByCode(nomination.status.code);
+            info.push(eli.build('span', {
+                className: classNameInfo,
+            }, [
+                eli.icon(service.status.types.get(type).icon),
+                eli.build('span', {
+                    className: 'margin-l--4',
+                    innerHTML: nomination.resultDateString
+                }),
+            ]));
+            info.push(eli.build('span', {
+                className: `${classNameInfo} status-${type}`,
+            }, [
+                eli.icon(nomination.status.icon),
+                eli.build('span', {
+                    className: 'margin-l--4',
+                    innerHTML: i18next.t(nomination.status.title)
+                }),
+            ]));
+        }
+        const cssTextImg = [
+            'object-fit: cover',
+            'object-position: center',
+            'width: 120px',
+            'min-width: 120px',
+            'height: 120px',
+            'min-height: 120px',
+        ].join(';');
+        const element = eli.build('div', {
+            className: 'margin-v--8 flex-box-row--nowrap'
+        }, [
+            eli.build('img', {
+                cssText: cssTextImg,
+                src: nomination.imageUrl.length > 0 ? nomination.imageUrl : 'https://wayfarer.nianticlabs.com/img/missing_image.png'
+            }),
+            eli.build('div', {
+                className: [
+                    'padding--8',
+                    'flex-box--col',
+                    'flex-align-items--start',
+                    'flex-justify-content--start'
+                ].join(' '),
+            }, [
+                eli.build('span', {
+                    className: 'mdc-typography--headline6',
+                    innerHTML: nomination.title,
+                }),
+                eli.build('div', {
+                    className: 'mdc-typography--body2 flex-box-row--wrap',
+                }, info),
+            ]),
+        ]);
+        this.content.append(element);
     }
 };
 
