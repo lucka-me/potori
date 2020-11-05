11520961585e893403a935451eaac9f7234ebf71
lucka-me <i@lucka.moe>
2020-11-05 13:52:25 +0800

    Update style

--- src/ui/app-bar/menu/constants.ts
+++ src/ui/app-bar/menu/constants.ts
@@ -2,10 +2,10 @@
  * Items in app bar menu
  */
 export const AppBarMenuItems = {
-    open     : { key: 'open'    , title: 'ui.app-bar.menu.open'       },
-    save     : { key: 'save'    , title: 'ui.app-bar.menu.save'       },
-    upload   : { key: 'upload'  , title: 'ui.app-bar.menu.upload'     },
-    import   : { key: 'import'  , title: 'ui.app-bar.menu.import'     },
-    about    : { key: 'about'   , title: 'ui.app-bar.menu.about'      },
-    signout  : { key: 'signout' , title: 'ui.app-bar.menu.signOut'   },
+    open     : { key: 'open'    , title: 'ui.app-bar.menu.open'     },
+    save     : { key: 'save'    , title: 'ui.app-bar.menu.save'     },
+    upload   : { key: 'upload'  , title: 'ui.app-bar.menu.upload'   },
+    import   : { key: 'import'  , title: 'ui.app-bar.menu.import'   },
+    about    : { key: 'about'   , title: 'ui.app-bar.menu.about'    },
+    signout  : { key: 'signout' , title: 'ui.app-bar.menu.signOut'  },
 };
