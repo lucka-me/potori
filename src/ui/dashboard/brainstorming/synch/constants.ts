c902df201147b7878d96ccad85b999caa2905efb
lucka-me <i@lucka.moe>
2020-11-06 16:59:02 +0800

    Remove unused strings

--- src/ui/dashboard/brainstorming/synch/constants.ts
+++ src/ui/dashboard/brainstorming/synch/constants.ts
@@ -3,7 +3,4 @@ const StringKeyBase = 'ui.dashboard.brainstorming.synch';
 export const StringKey = {
     title: `${StringKeyBase}.title`,
     desc: `${StringKeyBase}.desc`,
-
-    refresh: `${StringKeyBase}.refresh`,
-    clear: `${StringKeyBase}.clear`,
 };
