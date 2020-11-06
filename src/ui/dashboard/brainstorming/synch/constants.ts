fab4a01c62158ba033c8c34ca42c06453a7677e0
lucka-me <i@lucka.moe>
2020-11-06 16:19:42 +0800

    Use new eli, scss and constants

---
+++ src/ui/dashboard/brainstorming/synch/constants.ts
@@ -0,0 +1,9 @@
+const StringKeyBase = 'ui.dashboard.brainstorming.synch';
+
+export const StringKey = {
+    title: `${StringKeyBase}.title`,
+    desc: `${StringKeyBase}.desc`,
+
+    refresh: `${StringKeyBase}.refresh`,
+    clear: `${StringKeyBase}.clear`,
+};
--- src/ui/dashboard/brainstorming/synch/index.ts
+++ src/ui/dashboard/brainstorming/synch/index.ts
@@ -1,53 +1,48 @@
 import i18next from 'i18next';
 
-import { eli } from 'ui/eli';
+import { eli } from 'eli/eli';
+import { eliCard } from 'eli/card';
+
 import { BrainstormingStats } from 'service/brainstorming';
 import { base } from 'ui/dashboard/brainstorming/base';
 
 import './style.scss';
 
+import { StringKey } from './constants';
+
 class BSSynchCard extends base.ChartCardPrototype {
 
-    textSynch: HTMLSpanElement = null;
-
     render() {
-        this.textSynch = eli.build('span', {
-            cssText: 'font-weight:300;font-size:6rem;line-height:6rem;',
-            innerHTML: '0.0',
-        })
-        this.root = eli.build('div', {
-            className: [
-                'mdc-card',
-                'mdc-card--outlined',
-                'padding--8',
-                'flex--1',
-                'flex-shrink--1',
-                'flex-justify-content--between'
-            ].join(' '),
-        }, [
-            eli.build('span', {
-                className: 'mdc-typography--headline6',
-                innerHTML: i18next.t('ui.dashboard.brainstorming.synch.title'),
-            }),
-            eli.build('span', { className: 'text-nowarp' }, [
-                this.textSynch,
-                eli.build('span', {
-                    cssText: 'font-size:2rem;line-height:2rem;',
-                    innerHTML: '%'
+        this.root = eliCard('bs-synch-card', [
+            eli('div', { className: 'content' }, [
+                eli('span', {
+                    className: 'title',
+                    innerHTML: i18next.t(StringKey.title),
                 }),
+                eli('span', { className: 'synch' }, [
+                    eli('span', {
+                        className: 'number',
+                        innerHTML: '0.0',
+                    }),
+                    eli('span', {
+                        className: 'percent',
+                        innerHTML: '%'
+                    }),
+                ]),
+                eli('span', {
+                    className: 'desc',
+                    innerHTML: i18next.t(StringKey.desc),
+                })
             ]),
-            eli.build('span', {
-                className: 'mdc-typography--body1 text-nowarp',
-                innerHTML: i18next.t('ui.dashboard.brainstorming.synch.desc'),
-            }),
         ]);
+
         this.setVisible(false);
         this.parent.append(this.root);
     }
 
     updateStats(stats: BrainstormingStats) {
         const rate = stats.synch.total > 0 ? stats.synch.synched / stats.synch.total : 0.0;
-        this.textSynch.innerHTML = `${(rate * 100).toFixed(1)}`;
+        this.root.querySelector('.number').innerHTML = `${(rate * 100).toFixed(1)}`;
     }
 }
 
