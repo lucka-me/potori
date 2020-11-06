109919858bf8c8f27a285929ef1b51c0eff56da8
lucka-me <i@lucka.moe>
2020-11-06 10:00:57 +0800

    Use eliDialog

--- src/ui/dialog/import/index.ts
+++ src/ui/dialog/import/index.ts
@@ -2,7 +2,8 @@ import i18next from 'i18next';
 import { MDCDialog } from '@material/dialog';
 import { MDCTextField } from '@material/textfield';
 
-import { eli } from 'ui/eli';
+import { eli } from 'eli/eli';
+import { eliDialog } from 'eli/dialog';
 import { base } from 'ui/dialog/base';
 
 import './style.scss'
@@ -18,59 +19,52 @@ export default class ImportDialog extends base.DialogPrototype {
     import: ImportCallback = () => { }; // Triggered when click Import button
 
     render() {
-        const elementTextField = eli.build('div', {
+        const elementTextField = eli('div', {
             className: ClassName.textfield,
         }, [
-            eli.build('textarea', {
+            eli('textarea', {
                 className: 'mdc-text-field__input code',
                 id: 'input-dialog-import-wayfarer',
                 rows: 8, cols: 80
             }),
-            eli.build('div', {
+            eli('div', {
                 className: 'mdc-notched-outline',
             }, [
-                eli.build('div', { className: 'mdc-notched-outline__leading' }),
-                eli.build('div', {
+                eli('div', { className: 'mdc-notched-outline__leading' }),
+                eli('div', {
                     className: 'mdc-notched-outline__notch',
                 }, [
-                    eli.build('label', {
+                    eli('label', {
                         className: 'mdc-floating-label',
                         for: 'input-dialog-import-wayfarer',
                         innerHTML: i18next.t(StringKey.json),
                     }),
                 ]),
-                eli.build('div', { className: 'mdc-notched-outline__trailing' }),
+                eli('div', { className: 'mdc-notched-outline__trailing' }),
             ]),
         ]);
-        const elementDialog = base.buildDialog('', [
-            eli.build('h2', {
-                className: base.ClassName.title,
-                innerHTML: i18next.t(StringKey.title)
-            }),
-            eli.build('div', {
-                className: base.ClassName.content,
-            }, [
+        const elementDialog = eliDialog('import-dialog', {
+            title: i18next.t(StringKey.title),
+            contents: [
                 elementTextField,
-                eli.build('div', {
+                eli('div', {
                     className: 'mdc-text-field-helper-line',
                 }, [
-                    eli.build('div', {
+                    eli('div', {
                         className: 'mdc-text-field-helper-text mdc-text-field-helper-text--persistent',
                     }, [
                         i18next.t(StringKey.from),
-                        base.buildLink(
-                            Link.wayfarer, i18next.t(StringKey.wayfarer),
+                        eliDialog.link(
+                            i18next.t(StringKey.wayfarer), Link.wayfarer
                         ),
                     ]),
                 ]),
-            ]),
-            eli.build('footer', {
-                className: base.ClassName.actions,
-            }, [
-                base.buildDialogAction(base.Action.close , i18next.t(base.StringKey.close)),
-                base.buildDialogAction(Action.import, i18next.t(StringKey.import)),
-            ]),
-        ]);
+            ],
+            actions: [
+                { action: base.Action.close, text: i18next.t(base.StringKey.close) },
+                { action: Action.import, text: i18next.t(StringKey.import) },
+            ]
+        });
         this.parent.append(elementDialog);
         this.ctrl = new MDCDialog(elementDialog);
         this.textField = new MDCTextField(elementTextField);
