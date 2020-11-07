cbe2145b1bbf9c4a85333323656b544604af21a7
lucka-me <samzhao0727@gmail.com>
2020-11-07 23:39:37 +0800

    Add and use eliLinearProgress

---
+++ src/ui/eli/linear-progress.ts
@@ -0,0 +1,32 @@
+import { eli } from './eli';
+
+const ClassName = {
+    linearProgress: 'mdc-linear-progress',
+    buffer: 'mdc-linear-progress__buffer',
+    bufferBar: 'mdc-linear-progress__buffer-bar',
+    bufferDots: 'mdc-linear-progress__buffer-dots',
+    primaryBar: 'mdc-linear-progress__bar mdc-linear-progress__primary-bar',
+    secondaryBar: 'mdc-linear-progress__bar mdc-linear-progress__secondary-bar',
+    inner: 'mdc-linear-progress__bar-inner',
+};
+
+export function eliLinearProgress() {
+    return eli('div', {
+        className: ClassName.linearProgress,
+    }, [
+        eli('div', { className: ClassName.buffer }, [
+            eli('div', { className: ClassName.bufferBar }),
+            eli('div', { className: ClassName.bufferDots }),
+        ]),
+        eli('div', {
+            className: ClassName.primaryBar,
+        }, [
+            eli('span', { className: ClassName.inner }),
+        ]),
+        eli('div', {
+            className: ClassName.secondaryBar,
+        }, [
+            eli('span', { className: ClassName.inner }),
+        ]),
+    ]);
+}
--- src/ui/progress/index.ts
+++ src/ui/progress/index.ts
@@ -1,6 +1,6 @@
 import { MDCLinearProgress } from '@material/linear-progress';
 
-import { eli } from 'ui/eli';
+import { eliLinearProgress } from 'eli/linear-progress';
 import UIPrototype from 'ui/base';
 
 import './style.scss';
@@ -19,27 +19,11 @@ class Progress extends UIPrototype {
     }
 
     render() {
-        this.root = eli.build('div', {
-            className: 'mdc-linear-progress mdc-linear-progress--closed',
-            hidden: true
-        }, [
-            eli.build('div', { className: 'mdc-linear-progress__buffer' }, [
-                eli.build('div', { className: 'mdc-linear-progress__buffer-bar' }),
-                eli.build('div', { className: 'mdc-linear-progress__buffer-dots' }),
-            ]),
-            eli.build('div', {
-                className: 'mdc-linear-progress__bar mdc-linear-progress__primary-bar',
-            }, [
-                eli.build('span', { className: 'mdc-linear-progress__bar-inner' }),
-            ]),
-            eli.build('div', {
-                className: 'mdc-linear-progress__bar mdc-linear-progress__secondary-bar',
-            }, [
-                eli.build('span', { className: 'mdc-linear-progress__bar-inner' }),
-            ]),
-        ]);
+        this.root = eliLinearProgress();
+        this.root.hidden = true;
         this.parent.append(this.root);
         this.ctrl = new MDCLinearProgress(this.root);
+        this.ctrl.close();
     }
 
     /**
