2e7062ca67c51d747260168a7efa83cdad67ba82
lucka-me <i@lucka.moe>
2020-11-09 15:56:41 +0800

    Add dismiss action

--- src/ui/eli/snackbar.ts
+++ src/ui/eli/snackbar.ts
@@ -1,19 +1,30 @@
 import { eli } from './eli';
+import { eliIcon } from './icon';
 
 const ClassName = {
     snackbar: 'mdc-snackbar',
     surface: 'mdc-snackbar__surface',
     label: 'mdc-snackbar__label',
+    actions: 'mdc-snackbar__actions',
+    dismiss: 'mdc-snackbar__dismiss mdc-icon-button fa',
 };
 
 export function eliSnackbar() {
     return eli('div', { className: ClassName.snackbar }, [
-        eli('div', { className: ClassName.surface }, [
+        eli('div', {
+            className: ClassName.surface,
+            role: 'status',
+            ariaRelevant: 'additions',
+        }, [
+            eli('div', { className: ClassName.label }),
             eli('div', {
-                className: ClassName.label,
-                role: 'status',
-                ariaLive: 'polite',
-            }),
+                className: ClassName.actions,
+            }, [
+                eli('button', {
+                    className: ClassName.dismiss,
+                    innerHTML: eliIcon.Icon.times
+                })
+            ])
         ]),
     ]);
 }
