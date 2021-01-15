5c2d5e426d87d8a7862e47d25a4b6ad760b549e4
lucka-me <i@lucka.moe>
2021-01-15 16:47:00 +0800

    Remove debug code

--- scripts/post-build.ts
+++ scripts/post-build.ts
@@ -1,7 +1,4 @@
 import { execSync } from 'child_process';
 
-console.log(execSync('ls', {
-    encoding: "utf-8"
-}))
 execSync('sed -i "s/\\"build\\": \\"[[:digit:]]\\+\\",/\\"build\\": \\"__BUILD_NUMBER__\\",/" package.json');
 console.log(`Set build to __BUILD_NUMBER__ in package.json.`);
