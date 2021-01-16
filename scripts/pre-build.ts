1ce7516d0fa05f06a1fb067a55e901f57321456f
lucka-me <samzhao0727@gmail.com>
2021-01-16 16:38:41 +0800

    Get commit count in better way

--- scripts/pre-build.ts
+++ scripts/pre-build.ts
@@ -2,7 +2,7 @@ import { execSync } from 'child_process';
 import { locale } from './build-locales';
 
 locale.build();
-const build = execSync('git rev-list main | wc -l', {
+const build = execSync('git rev-list --count HEAD', {
     encoding: "utf-8"
 }).trim();
 execSync(`sed -i "s/__BUILD_NUMBER__/${build}/" package.json`);
