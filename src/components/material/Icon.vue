518b176c3ea75c7352fa8c577413894fbeb26a67
lucka-me <i@lucka.moe>
2021-03-06 22:45:04 +0800

    Add fa icon and icon button

---
+++ src/components/material/Icon.vue
@@ -0,0 +1,53 @@
+<template>
+{{ symbol }}
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+const IconSymbolMap: Record<string, string> = {
+    'search': '\uf002',
+    'th-list'   : '\uf00b',
+    'times' : '\uf00d',
+    'cog'   : '\uf013',
+    'clock' : '\uf017',
+    'redo'  : '\uf01e',
+    'edit'  : '\uf044',
+    'info-circle'   : '\uf05a',
+    'arrow-up'  : '\uf062',
+    'plus'  : '\uf067',
+    'calendar-alt'  : '\uf073',
+    'folder-open'   : '\uf07c',
+    'globe' : '\uf0ac',
+    'angle-up'  : '\uf106',
+    'angle-down': '\uf107',
+    'ellipsis-v': '\uf142',
+    'trash' : '\uf1f8',
+    'sync-alt'  : '\uf2f1',
+    'sign-in-alt'   : '\uf2f6',
+    'redo-alt'  : '\uf2f9',
+    'map-marker-alt': '\uf3c5',
+    'tachometer-alt': '\uf3fd',
+    'brain' : '\uf5dc',
+}
+
+@Options({
+    props: {
+        name: String
+    },
+})
+export default class MaterialIcon extends Vue {
+    name!: string;
+    get symbol() {
+        return IconSymbolMap[this.name];
+    }
+}
+</script>
+
+<style lang="scss">
+$fa-font-path: '~@fortawesome/fontawesome-free/webfonts';
+@import '~@fortawesome/fontawesome-free/scss/solid';
+@import '~@fortawesome/fontawesome-free/scss/mixins';
+@import '~@fortawesome/fontawesome-free/scss/core';
+@import '~@fortawesome/fontawesome-free/scss/fixed-width';
+</style>
---
+++ src/components/material/IconButton.vue
@@ -0,0 +1,46 @@
+<template>
+<button class="fa mdc-icon-button">
+    <material-icon :name="icon"/>
+</button>
+</template>
+
+<script lang="ts">
+import { MDCRipple } from '@material/ripple';
+import { Options, Vue } from 'vue-class-component';
+
+import MaterialIcon from './Icon.vue';
+
+@Options({
+    props: {
+        icon: String
+    },
+    components: {
+        MaterialIcon
+    },
+})
+export default class MaterialTopAppBar extends Vue {
+
+    private ctrl?: MDCRipple;
+
+    mounted() {
+        this.ctrl = MDCRipple.attachTo(this.$el);
+        this.ctrl.unbounded = true;
+    }
+
+    updated() {
+        this.ctrl?.destroy();
+        this.ctrl = MDCRipple.attachTo(this.$el);
+        this.ctrl.unbounded = true;
+    }
+
+    unmounted() {
+        this.ctrl?.destroy();
+    }
+}
+</script>
+
+<style lang="scss">
+@use '~@material/icon-button';
+
+@include icon-button.core-styles;
+</style>
