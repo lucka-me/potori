cdf0cf267deba37f872002932c4a0ce44ffed330
lucka-me <i@lucka.moe>
2021-03-06 20:31:57 +0800

    Fill nomination list with simple contents

--- src/views/NominationList.vue
+++ src/views/NominationList.vue
@@ -1,13 +1,18 @@
 <template>
 <div class="nomination-list">
-    Nominations
+    {{ title }}
     |
     <router-link to="../">Back</router-link>
-    <hr/>
+    <hr>
+    <div v-for="nomination in nominations" :key="nomination.id" @click="open(nomination.id)">
+        {{ nomination.title }}
+    </div>
 </div>
 </template>
 
 <script lang="ts">
+import Nomination from '@/service/nomination';
+import { umi } from '@/service/umi';
 import { Options, Vue } from 'vue-class-component';
 
 @Options({
@@ -16,5 +21,44 @@ import { Options, Vue } from 'vue-class-component';
     },
 })
 export default class NominationList extends Vue {
+    get title(): string {
+        return this.commonSense?.title || 'All';
+    }
+
+    get nominations(): Array<Nomination> {
+        let list = this.$store.state.nominations;
+        const commonSense = this.commonSense;
+        if (commonSense) list = list.filter(commonSense.predicator);
+        list = list.sort(Nomination.comparatorByTime);
+        return list;
+    }
+
+    private get commonSense(): umi.CommonSense | null {
+        if (this.$route.query.status && typeof(this.$route.query.status) === 'string') {
+            const code = parseInt(this.$route.query.status);
+            return umi.status.get(code) || null;
+        }
+        if (this.$route.query.reason && typeof(this.$route.query.reason) === 'string') {
+            const code = parseInt(this.$route.query.reason);
+            console.log(code);
+            return umi.reason.get(code) || null;
+        }
+        if (this.$route.query.scanner && typeof(this.$route.query.scanner) === 'string') {
+            const code = parseInt(this.$route.query.scanner);
+            return umi.scanner.get(code) || null;
+        }
+        return null;
+    }
+
+    open(id: string) {
+        this.$router.push({
+            path: '/details',
+            query: { id: id }
+        });
+    }
 }
 </script>
+
+<style lang="scss">
+
+</style>
