6ea2d8f663aab949dc1a5f4bdb70b7881e20e8ce
lucka-me <i@lucka.moe>
2021-03-06 19:54:03 +0800

    Update mari

--- src/service/mari/index.ts
+++ src/service/mari/index.ts
@@ -1,12 +1,11 @@
-import i18next from 'i18next';
+import { umi } from '@/service/umi';
+import Nomination from '@/service/nomination';
 
-import { umi } from 'service/umi';
-import Nomination from 'service/nomination';
+import { parser } from './parser';
 
-import Parser, { QueryKeys } from './tools';
-
+type BasicCallback = () => void;
 type MessageCallback = (message: string) => void;
-type FinishCallback = () => void;
+type FinishCallback = (nominations: Array<Nomination>) => void;
 export type ProgressCallback = (percent: number) => void;
 
 /**
@@ -56,7 +55,7 @@ class Progress {
 
     onBuffer    : ProgressCallback  = () => { };    // Triggered when a list finished
     onProgress  : ProgressCallback  = () => { };    // Triggered when a message finished if all lists are finished
-    onFinish    : FinishCallback    = () => { };    // Triggered when all lists and messages are finished
+    onFinish    : BasicCallback     = () => { };    // Triggered when all lists and messages are finished
 
     /**
      * Clear all progress
@@ -135,7 +134,7 @@ export default class Mari {
             this.events.progress(percent);
         }
         this.progress.onFinish = () => {
-            this.events.finish();
+            this.events.finish(this.nominations);
         }
     }
 
@@ -144,66 +143,69 @@ export default class Mari {
      * @param nominations Existing nominations
      */
     start(nominations: Array<Nomination>) {
-        this.nominations = nominations;
-
+        console.log('Mari start');
+        this.nominations = nominations.map(nomination => nomination);
         this.progress.lists.clear();
         this.progress.messages.clear();
 
         // Ignore the mails already in the list
-        this.ignoreMailIds  = [];
-        for (const nomination of this.nominations) {
-            this.ignoreMailIds.push(nomination.confirmationMailId);
-            if (nomination.resultMailId) this.ignoreMailIds.push(nomination.resultMailId);
-        }
+        this.ignoreMailIds = this.nominations.flatMap(nomination => {
+            return nomination.resultMailId.length > 0 ? [ nomination.confirmationMailId, nomination.resultMailId ] : [ nomination.confirmationMailId ];
+        });
         for (const status of umi.status.values()) {
             for (const scanner of status.queries.keys()) {
-                this.queryList({ status: status.code, scanner: scanner });
+                this.queryList(status.code, scanner);
             }
         }
     }
 
     /**
      * Start to query message list
-     * @param keys Query keys for the list
+     * @param status Status to query
+     * @param scanner Scanner to query
      */
-    private queryList(keys: QueryKeys) {
+    private queryList(status: umi.StatusCode, scanner: umi.ScannerCode) {
         this.progress.addList();
-        const listRequest = Mari.getListRequest(null, keys);
+        const listRequest = Mari.getListRequest(undefined, status, scanner);
         listRequest.execute((response) => {
-            this.handleListRequest(response, keys, []);
+            this.handleListRequest(response, [], status, scanner);
         });
     }
 
     /**
      * Generate a request for mail list
      * @param pageToken Token for target page
-     * @param keys Query keys to get query string
+     * @param status Status to query
+     * @param scanner Scanner to query, must exist in the `queries` of the status
      */
-    private static getListRequest(pageToken: string, keys: QueryKeys) {
+    private static getListRequest(pageToken: string | undefined, status: umi.StatusCode, scanner: umi.ScannerCode) {
         return gapi.client.gmail.users.messages.list({
-            'userId': 'me',
-            'q': umi.status.get(keys.status).queries.get(keys.scanner),
-            'pageToken': pageToken
+            userId: 'me',
+            q: umi.status.get(status)!.queries.get(scanner)!,
+            pageToken: pageToken
         });
     }
 
     /**
      * Handle the respose of mail list request, request next page or goto next step
      * @param response Response of the request
-     * @param keys Keys of the scanner-type group
      * @param list Mail list
+     * @param status Status of the list
+     * @param scanner Scanner of the list
      */
     private handleListRequest(
         response: gapi.client.Response<gapi.client.gmail.ListMessagesResponse>,
-        keys: QueryKeys, list: Array<gapi.client.gmail.Message>
+        list: Array<gapi.client.gmail.Message>,
+        status: umi.StatusCode,
+        scanner: umi.ScannerCode
     ) {
         if (response.result.messages) {
             list.push(...response.result.messages);
         }
         if (response.result.nextPageToken) {
-            const request = Mari.getListRequest(response.result.nextPageToken, keys);
+            const request = Mari.getListRequest(response.result.nextPageToken, status, scanner);
             request.execute((newResponse) => {
-                this.handleListRequest(newResponse, keys, list);
+                this.handleListRequest(newResponse, list, status, scanner);
             });
         } else {
             for (let i = list.length - 1; i >= 0; i--) {
@@ -214,19 +216,21 @@ export default class Mari {
                     }
                 }
             }
-            this.queryMessages(keys, list);
+            this.queryMessages(list, status, scanner);
         }
     }
 
     /**
      * Process mail (id) list
-     * @param keys Keys of the scanner-type group
      * @param list Complete mail list
+     * @param status Status of the list
+     * @param scanner Scanner of the list
      */
-    private queryMessages(keys: QueryKeys, list: Array<gapi.client.gmail.Message>) {
+    private queryMessages(list: Array<gapi.client.gmail.Message>, status: umi.StatusCode, scanner: umi.ScannerCode) {
         this.progress.finishList(list.length);
 
         for (const mail of list) {
+            if (!mail.id) continue;
             gapi.client.gmail.users.messages.get({
                 userId: 'me',
                 id: mail.id,
@@ -235,14 +239,13 @@ export default class Mari {
             }).execute((response: gapi.client.Response<gapi.client.gmail.Message>) => {
                 const fullMail = response.result;
                 try {
-                    const nomination = Parser.mail(fullMail, keys);
+                    const nomination = parser.parse(fullMail, status, scanner);
                     this.nominations.push(nomination);
-                } catch (error: Error | any) {
+                } catch (error) {
                     let subject = '';
-                    for (let i = 0; i < fullMail.payload.headers.length; i++) {
-                        const header = fullMail.payload.headers[i];
+                    for (const header of fullMail.payload!.headers!) {
                         if (header.name === 'Subject') {
-                            subject = header.value;
+                            subject = header.value!;
                             break;
                         }
                     }
@@ -251,10 +254,11 @@ export default class Mari {
                         const typedError = error as Error;
                         details = typedError.stack || typedError.message;
                     }
-                    this.events.alert(i18next.t('message:service.mari.reportParserError', {
-                        subject: subject,
-                        message: `[${keys.scanner}:${keys.status}]${details}`
-                    }));
+                    // this.events.alert(i18next.t('message:service.mari.reportParserError', {
+                    //     subject: subject,
+                    //     message: `[${keys.scanner}:${keys.status}]${details}`
+                    // }));
+                    this.events.alert(`message:service.mari.reportParserError ${subject} [${status}:${scanner}]${details}`);
                 }
 
                 this.progress.finishMessage();
