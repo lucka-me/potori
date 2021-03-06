b0e0c63b2c0858210dbcc7e78e4adf6ce1bae334
lucka-me <i@lucka.moe>
2021-03-06 18:56:05 +0800

    Re-add mdc

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,11 @@
 <router-view/>
 </template>
 
-<style>
+<style lang="scss">
+@use '~@material/typography';
+@import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap');
+
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
+    @include typography.base();
 }
 </style>
