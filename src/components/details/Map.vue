445a19e02e0d222d78e4fdff8d1c80a11c96829f
lucka-me <i@lucka.moe>
2021-03-09 16:25:01 +0800

    mapbox -> mapboxgl

--- src/components/details/Map.vue
+++ src/components/details/Map.vue
@@ -5,7 +5,7 @@
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 
-import type mapbox from 'mapbox-gl';
+import type mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 import { LngLat } from '@/service/nomination';
@@ -20,22 +20,22 @@ import { LngLat } from '@/service/nomination';
 })
 export default class DetailsMap extends Vue {
 
-    private ctrl?: mapbox.Map;
+    private ctrl?: mapboxgl.Map;
     lngLat!: LngLat;
 
     mounted() {
         import(
             /* webpackChunkName: 'mapbox' */
             'mapbox-gl'
-        ).then((mapbox) => {
-            mapbox.accessToken = 'pk.eyJ1IjoibHVja2EtbWUiLCJhIjoiY2p2NDk5NmRvMHFreTQzbzduemM1MHV4cCJ9.7XGmxnEJRoCDr-i5BBmBfw';
-            this.ctrl = new mapbox.Map({
+        ).then((mapboxgl) => {
+            mapboxgl.accessToken = 'pk.eyJ1IjoibHVja2EtbWUiLCJhIjoiY2p2NDk5NmRvMHFreTQzbzduemM1MHV4cCJ9.7XGmxnEJRoCDr-i5BBmBfw';
+            this.ctrl = new mapboxgl.Map({
                 container: this.$el,
                 style: 'mapbox://styles/mapbox/outdoors-v11',
                 center: this.lngLat,
                 zoom: 16
             });
-            new mapbox.Marker().setLngLat(this.lngLat).addTo(this.ctrl);
+            new mapboxgl.Marker().setLngLat(this.lngLat).addTo(this.ctrl);
         });
     }
 }
