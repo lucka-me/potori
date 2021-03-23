5b21e3e57abcaa1a401b1ec581b86aa75999178e
lucka-me <i@lucka.moe>
2021-03-23 14:04:24 +0800

    Fix full version

--- src/service/version/index.ts
+++ src/service/version/index.ts
@@ -5,7 +5,7 @@ import umiJSON from '@/data/umi/umi.json';
  * Version information
  */
 export namespace version {
-    export const full = document.URL.includes('lucka.moe');
+    export const full = !document.URL.includes('lucka.moe');
     export const text = `${versionJSON.version}-${full ? 'lite' : 'full'} (${versionJSON.build})`;
     export const data = umiJSON.version;
 }
