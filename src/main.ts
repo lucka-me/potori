c09137d802a67a44a3eb47d0f41fe1f088659e7c
lucka-me <i@lucka.moe>
2021-03-24 17:13:27 +0800

    Add vue-i18n

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { createApp } from 'vue';
+import { createI18n } from 'vue-i18n';
 
+import { service } from './service';
 import App from './App.vue';
+import locales from './locales';
 import router from './router';
 import store from './store';
-import { service } from './service';
+
+const i18n = createI18n({
+    locale: navigator.language,
+    fallbackLocale: 'en',
+    messages: locales
+});
 
 service.init(store);
 createApp(App)
     .use(store)
     .use(router)
+    .use(i18n)
     .mount('#app');
