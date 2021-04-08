808e09bb9e4643628f2f73f046b3c9809332d8f6
lucka-me <lucka-me@users.noreply.github.com>
2021-04-08 06:54:56 +0000

    deploy: d7c7aa690d70b996e0347a7606a378155cb7b825

---
+++ next/precache-manifest.b50d6ba219f6b4cb7549fd469f92f858.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27cce0627739fb93c3b64dccc21a6641",
+    "url": "/next/assets/icon-maskable.png"
+  },
+  {
+    "revision": "e86f96fa9b28e101cda9a41fc058c748",
+    "url": "/next/assets/icon-rounded.png"
+  },
+  {
+    "revision": "e6f1deb8cbab8dc84b045351406ae5d3",
+    "url": "/next/assets/icon-transparent.png"
+  },
+  {
+    "revision": "fc5d576aff07f727ae97c5051a2b85ec",
+    "url": "/next/assets/title.png"
+  },
+  {
+    "revision": "273bc6083d5dc799f440",
+    "url": "/next/css/app.797a826b.css"
+  },
+  {
+    "revision": "ad7d8bbd2ad76aed92fb",
+    "url": "/next/css/brainstorming.2454f1eb.css"
+  },
+  {
+    "revision": "764f38d47d5b53f7c01c",
+    "url": "/next/css/brainstorming~charts.5109a264.css"
+  },
+  {
+    "revision": "4d03242ff56f0778c505",
+    "url": "/next/css/brainstorming~charts~dashboard~details~list~map~match~preferences.456360f3.css"
+  },
+  {
+    "revision": "fb173c9be46c2aab6144",
+    "url": "/next/css/brainstorming~dashboard.abfe8bd8.css"
+  },
+  {
+    "revision": "9b9aeb25d098cbfaa5eb",
+    "url": "/next/css/charts.a8695c17.css"
+  },
+  {
+    "revision": "f497d9f47396fce0b0ed",
+    "url": "/next/css/dashboard.c8a23613.css"
+  },
+  {
+    "revision": "b3ec70df7c30548a7945",
+    "url": "/next/css/details.e45c7c88.css"
+  },
+  {
+    "revision": "9bec2e5cdf67b6751880",
+    "url": "/next/css/details~map.8a324763.css"
+  },
+  {
+    "revision": "0c09c53e8b16d5394002",
+    "url": "/next/css/list.6a9f041e.css"
+  },
+  {
+    "revision": "4f6b220af1d356296d10",
+    "url": "/next/css/map.dc687b38.css"
+  },
+  {
+    "revision": "f47d09ddf356d6ec33d5",
+    "url": "/next/css/match.4e5ee524.css"
+  },
+  {
+    "revision": "e8e5f7c0bf8172179d90",
+    "url": "/next/css/preferences.a2815486.css"
+  },
+  {
+    "revision": "1042e8ca1ce821518a2d3e7055410839",
+    "url": "/next/fonts/fa-solid-900.1042e8ca.eot"
+  },
+  {
+    "revision": "605ed7926cf39a2ad5ec2d1f9d391d3d",
+    "url": "/next/fonts/fa-solid-900.605ed792.ttf"
+  },
+  {
+    "revision": "9fe5a17c8ab036d20e6c5ba3fd2ac511",
+    "url": "/next/fonts/fa-solid-900.9fe5a17c.woff"
+  },
+  {
+    "revision": "e8a427e15cc502bef99cfd722b37ea98",
+    "url": "/next/fonts/fa-solid-900.e8a427e1.woff2"
+  },
+  {
+    "revision": "376c1f97f6553dea1ca9b3f9081889bd",
+    "url": "/next/img/fa-solid-900.376c1f97.svg"
+  },
+  {
+    "revision": "9799e3930dc8603224740d78ff209832",
+    "url": "/next/index.html"
+  },
+  {
+    "revision": "273bc6083d5dc799f440",
+    "url": "/next/js/app.2baac58c.js"
+  },
+  {
+    "revision": "ad7d8bbd2ad76aed92fb",
+    "url": "/next/js/brainstorming.08ec32dc.js"
+  },
+  {
+    "revision": "764f38d47d5b53f7c01c",
+    "url": "/next/js/brainstorming~charts.5c6860e1.js"
+  },
+  {
+    "revision": "4d03242ff56f0778c505",
+    "url": "/next/js/brainstorming~charts~dashboard~details~list~map~match~preferences.8cbc9ed7.js"
+  },
+  {
+    "revision": "fb173c9be46c2aab6144",
+    "url": "/next/js/brainstorming~dashboard.dc8ca4ab.js"
+  },
+  {
+    "revision": "9b9aeb25d098cbfaa5eb",
+    "url": "/next/js/charts.498517d7.js"
+  },
+  {
+    "revision": "90ada1cc6d2cfbd1b702",
+    "url": "/next/js/chunk-vendors.6192948d.js"
+  },
+  {
+    "revision": "f497d9f47396fce0b0ed",
+    "url": "/next/js/dashboard.19c87d27.js"
+  },
+  {
+    "revision": "b3ec70df7c30548a7945",
+    "url": "/next/js/details.f6e6567e.js"
+  },
+  {
+    "revision": "9bec2e5cdf67b6751880",
+    "url": "/next/js/details~map.df86ac01.js"
+  },
+  {
+    "revision": "03bb753714c114d36fee",
+    "url": "/next/js/firebase.6f230759.js"
+  },
+  {
+    "revision": "0c09c53e8b16d5394002",
+    "url": "/next/js/list.f3f1f9a5.js"
+  },
+  {
+    "revision": "4f6b220af1d356296d10",
+    "url": "/next/js/map.1edfc995.js"
+  },
+  {
+    "revision": "a863def486a83b012b0c",
+    "url": "/next/js/mapbox.5d5a8588.js"
+  },
+  {
+    "revision": "f47d09ddf356d6ec33d5",
+    "url": "/next/js/match.8f408d69.js"
+  },
+  {
+    "revision": "e8e5f7c0bf8172179d90",
+    "url": "/next/js/preferences.e8c8fb31.js"
+  },
+  {
+    "revision": "e740a533dfc63e89adcc986fa213f9c3",
+    "url": "/next/manifest.json"
+  }
+]);
